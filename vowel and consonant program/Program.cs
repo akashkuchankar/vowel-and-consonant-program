e03ede935a71a2de67a19326d44073182ff9f3fc
akashkuchankar <kuchankarakash@gmail.com>
2022-10-04 05:11:03 -0700

    Add project files.

---
+++ vowel and consonant program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowel_and_consonant_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any alphabet");
+            char ak = Convert.ToChar(Console.ReadLine());
+            switch(ak)
+            {
+                case 'a':
+                        Console.WriteLine("alphabet is vowel");
+                    break;  
+                    case 'e':
+                    Console.WriteLine("alphabet is vowel");
+                    break ;
+                    case 'i':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                    case'o':
+                    Console.WriteLine("alphabet is vowel");
+                    break;
+                    default:
+                    Console.WriteLine("alphabet is consonant");
+                    break;
+                    Console.ReadLine();
+            }
+
+        }
+    }
+}
